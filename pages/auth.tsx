227cf48b49256ce05aaa6f6cb2089f19a001bb68
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-04-12 12:25:42 +0300

    settings

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,27 +1,50 @@
-import Input from "@/components/Input";
+import Input from "@/components/input";
+import { useState } from "react";
 
 const Auth = () => {
-    return (
-        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            <div className="bg-black bg-opacity-50 w-full h-full" >
-                <nav className="px-12 py-5">
-                    <img src="/images/logo.png" className="h-12" alt="Logo" />
-                </nav>
-                <div className="flex justify-center">
-                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center">
-                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            {'Sign in'}
-                         </h2>
-                         <div className="flex flex-col gap-4">
-                            <Input />
-
-                         </div>
-
-                    </div>
-                </div>
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black bg-opacity-50 w-full h-full">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-12 py-12 self-center">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                id="name"
+                type="text"
+                label="Username"
+                value={name}
+                onChange={(ev: any) => setName(ev.target.value)}
+              />
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(ev: any) => setPassword(ev.target.value)}
+              />
             </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              Login
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
